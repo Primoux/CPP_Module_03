dcf6841b8c29659ab1e269801b4d9b8533e2baa0
enchevri <enzochevrier@icloud.com>
2026-01-02 08:00:06 +0100

    Update Claptrap class usage in main and fix comments

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 16:33:57 by enchevri          #+#    #+#             */
-/*   Updated: 2026/01/02 07:56:38 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2026/01/02 07:59:54 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 int	main(void)
 {
-	Claptrap c("Enzo");
-	Claptrap c2("Paolo");
-	c.setAttackPoints(10);
-	c.attack("Paolo");
+	Claptrap c("Enchevri");
+	Claptrap c2("Faoriol");
+	c.setAttackPoints(5);
+	c.attack("Faoriol");
 	c2.takeDamage(c.getAttackPoints());
+	c.attack("Faoriol");
 	c2.takeDamage(c.getAttackPoints());
 	return (0);
 }
